03923f6341a1592a361cf13f171abaca48ec0890
msymt <nam.nam580@gmail.com>
2021-08-21 14:38:37 +0900

    modfiy tab

--- src/psk31.py
+++ src/psk31.py
@@ -12,13 +12,13 @@ def main():
 
 	q=[];i=[]
 	for j in range(waveFile.getnframes()):
-				buf = waveFile.readframes(1)
-				# 符号なし→符号あり
-				q.append((buf[0]-128)*np.sin(np.pi*2.0/FQ*j))
-				i.append((buf[0]-128)*np.cos(np.pi*2.0/FQ*j))
-
-				print(int(sum(q)>0),int(sum(i)>0),sep=",")
-				if j>wind:q.pop(0);i.pop(0)
+		buf = waveFile.readframes(1)
+		# 符号なし→符号あり
+		q.append((buf[0]-128)*np.sin(np.pi*2.0/FQ*j))	#実部
+		i.append((buf[0]-128)*np.cos(np.pi*2.0/FQ*j))	#虚部
+		print(int(sum(q)>0),int(sum(i)>0),sep=",")
+		if j>wind:
+			q.pop(0);i.pop(0)
 	waveFile.close()
 
 if __name__ == "__main__":
